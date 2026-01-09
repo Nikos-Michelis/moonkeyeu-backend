9cc9bbba2769b2a328d5dfd754f22b6f1d867c46
Michelis Nikos <michelis.nikos36@gmail.com>
2026-01-09 22:51:04 +0200

    refactor: convert StepContextSetter to record

--- moonkeyeu-etl-api/src/main/java/com/moonkeyeu/etl/api/configuration/batch/listeners/StepContextSetter.java
+++ moonkeyeu-etl-api/src/main/java/com/moonkeyeu/etl/api/configuration/batch/listeners/StepContextSetter.java
@@ -1,35 +1,19 @@
 package com.moonkeyeu.etl.api.configuration.batch.listeners;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.item.ExecutionContext;
-
+/**
+ * @param key   Key to store in ExecutionContext
+ * @param value Value to store (can be Object or primitive wrapper)
+ */
 @Slf4j
-public class StepContextSetter<T> implements StepExecutionListener {
-
-    private final String key;
-    private final T value;
-
-    /**
-     * @param key   Key to store in ExecutionContext
-     * @param value Value to store (can be Object or primitive wrapper)
-     */
-    public StepContextSetter(String key, T value) {
-        this.key = key;
-        this.value = value;
-    }
+public record StepContextSetter<T>(String key, T value) implements StepExecutionListener {
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
         ExecutionContext context = stepExecution.getExecutionContext();
         context.put(key, value);
-        log.debug("Set key '{}' in step execution context with value: {}", key, value);
-    }
-
-    @Override
-    public ExitStatus afterStep(StepExecution stepExecution) {
-        return null;
     }
 }
